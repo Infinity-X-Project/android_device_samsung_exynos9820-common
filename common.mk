fa2e3f7842e3d5e1b70157213838c750e51edf44
FrEeRuNnEr4EvEr <ledrew101806@gmail.com>
2025-10-09 16:09:17 -0400

    exynos9820-common: Remove duplicate infinity x flags
    Reason: Flags are already defined in the device trees

--- common.mk
+++ common.mk
@@ -333,7 +333,3 @@ endif
 
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
-
-# Project Infinity X Flags
-INFINITY_BUILD_TYPE := OFFICIAL
-INFINITY_MAINTAINER := FrEeRuNnEr4EvEr
